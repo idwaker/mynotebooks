01c1f6626f1482dd57028a6387c834fc8b1dcaa0
Diwaker Ghimire <idwaker@gmail.com>
2019-04-01 20:47:37 +0545

    use dummy data for large analysis

---
+++ sql/dummy_departments.sql
@@ -0,0 +1,14 @@
+-- https://mockaroo.com/
+insert into departments (department, division) values ('Computers', 'Electronics');
+insert into departments (department, division) values ('Automotive', 'Hardware');
+insert into departments (department, division) values ('Books', 'Entertainment');
+insert into departments (department, division) values ('Shoes', 'Home');
+insert into departments (department, division) values ('Grocery', 'Home');
+insert into departments (department, division) values ('Beauty', 'Home');
+insert into departments (department, division) values ('Clothing', 'Home');
+insert into departments (department, division) values ('Games', 'Kids');
+insert into departments (department, division) values ('Music', 'Entertainment');
+insert into departments (department, division) values ('Garden', 'Outdoors');
+insert into departments (department, division) values ('Jewelery', 'Home');
+insert into departments (department, division) values ('Toys', 'Kids');
+insert into departments (department, division) values ('Movies', 'Entertainment');
---
+++ sql/dummy_regions.sql
@@ -0,0 +1,9 @@
+-- https://mockaroo.com/
+insert into regions (id, name, country) values (1, 'Quebec', 'Canada');
+insert into regions (id, name, country) values (2, 'Colorado', 'United States');
+insert into regions (id, name, country) values (3, 'Florida', 'United States');
+insert into regions (id, name, country) values (4, 'Texas', 'United States');
+insert into regions (id, name, country) values (5, 'Berlin', 'Germany');
+insert into regions (id, name, country) values (6, 'Alberta', 'Canada');
+insert into regions (id, name, country) values (7, 'Ontario', 'Canada');
+insert into regions (id, name, country) values (8, 'Hamburg', 'Germany');
